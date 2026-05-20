181664b3438a1f5b4e7bd0e5e33645bcdc87a362
hutrisemendawai <hutrisemendawai.gg@gmail.com>
2026-05-20 10:28:25 +0700

    Update to larvel 12

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -12,21 +12,32 @@ class RegistrationTest extends TestCase
 
     public function test_registration_screen_can_be_rendered(): void
     {
-        $response = $this->get('/register');
+        $adminUser = \App\Models\User::factory()->create(['role' => 'admin']);
+
+        $response = $this->actingAs($adminUser)->get('/register');
 
         $response->assertStatus(200);
     }
 
     public function test_new_users_can_register(): void
     {
-        $response = $this->post('/register', [
-            'name' => 'Test User',
+        $adminUser = \App\Models\User::factory()->create(['role' => 'admin']);
+
+        $response = $this->actingAs($adminUser)->post('/register', [
+            'first_name' => 'Test',
+            'last_name' => 'User',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'birth_date' => '2000-01-01',
+            'address' => 'Test address',
+            'phone_number' => '081234567890',
+            'role' => 'enum'
         ]);
 
-        $this->assertAuthenticated();
+        $this->assertDatabaseHas('users', [
+            'email' => 'test@example.com'
+        ]);
         $response->assertRedirect(route('dashboard'));
     }
 }
